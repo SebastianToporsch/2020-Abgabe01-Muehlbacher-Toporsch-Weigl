135b53b81646699b8c1b89792b798d9a9b89badd
weTobias <tobias.weigl99@gmail.com>
2020-05-09 16:03:48 +0200

    Create VodkaLongdrink.java
    Added VodkaLongdrink class with signature and description of VodkaLongdrink and it's constructor

---
+++ Drinks/src/main/java/at/fhj/iit/VodkaLongdrink.java
@@ -0,0 +1,12 @@
+package at.fhj.iit;
+
+/**
+ * VodkaLongdrink is a class derived from VodkaDrink.
+ * It represents a longdrink that is based on vodka.
+ */
+public class VodkaLongdrink {
+    /**
+     * Constructor for VodkaLongdrink
+     */
+    public VodkaLongdrink(){}
+}
