1ffa338e1590aeac144d46438e95773dd46d3e0c
weTobias <tobias.weigl99@gmail.com>
2020-05-09 17:55:22 +0200

    Update VodkaLongdrink.java
    Updated class description.
    Updated constructor description.
    Added variables and description for vodka and filler.

--- Drinks/src/main/java/at/fhj/iit/VodkaLongdrink.java
+++ Drinks/src/main/java/at/fhj/iit/VodkaLongdrink.java
@@ -3,10 +3,26 @@ package at.fhj.iit;
 /**
  * VodkaLongdrink is a class, that extends VodkaDrink.
  * It represents a longdrink that is based on vodka.
+ *
+ * @author Tobias Weigl
+ * @version %I%, %G%
  */
-public class VodkaLongdrink {
+public class VodkaLongdrink extends VodkaDrink{
     /**
-     * Constructor for VodkaLongdrink
+     * represents vodka in longdrink
      */
-    public VodkaLongdrink(){}
+    private Liquid vodka;
+    /**
+     * represents filler in longdrink
+     */
+    private Liquid filler;
+
+    /**
+     * Creates new longdrink with given name and liquids.
+     * Mixes the two given liquids into a new one.
+     *
+     */
+    public VodkaLongdrink(String name, Liquid vodka, Liquid filler){
+        
+    }
 }
