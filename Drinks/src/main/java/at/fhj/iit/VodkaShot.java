dfa6257e396bcd4beb929a0e70dadd698fab6f76
weTobias <tobias.weigl99@gmail.com>
2020-05-09 16:14:22 +0200

    Create VodkaShot.java
    Added VodkaShot class with signature and description of VodkaShot and its constructor

---
+++ Drinks/src/main/java/at/fhj/iit/VodkaShot.java
@@ -0,0 +1,12 @@
+package at.fhj.iit;
+
+/**
+ * VodkaShot is a class that extends VodkaDrink.
+ * It represents a pure vodka shot.
+ */
+public class VodkaShot {
+    /**
+     * Constructor for VodkaShot
+     */
+    public VodkaShot(){}
+}
