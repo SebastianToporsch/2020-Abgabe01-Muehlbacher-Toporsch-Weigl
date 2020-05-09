b72d29fe3de084d151f3c9c0dfe634694b3f3475
weTobias <tobias.weigl99@gmail.com>
2020-05-09 18:08:20 +0200

    Update VodkaShot.java
    VodkaShot:
    Updated class description

--- Drinks/src/main/java/at/fhj/iit/VodkaShot.java
+++ Drinks/src/main/java/at/fhj/iit/VodkaShot.java
@@ -3,6 +3,9 @@ package at.fhj.iit;
 /**
  * VodkaShot is a class that extends VodkaDrink.
  * It represents a pure vodka shot.
+ *
+ * @author Tobias Weigl
+ * @version %I%, %G%
  */
 public class VodkaShot {
     /**
