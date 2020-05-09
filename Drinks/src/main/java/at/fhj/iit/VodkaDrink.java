e9117eb463860f5d491b9e0e6bac39ba732afa32
weTobias <tobias.weigl99@gmail.com>
2020-05-09 14:26:13 +0200

    Create VodkaDrink.java
    Added VodkaDrink class with class description

---
+++ Drinks/src/main/java/at/fhj/iit/VodkaDrink.java
@@ -0,0 +1,16 @@
+package at.fhj.iit;
+
+/**
+ * VodkaDrink is the base abstract class for all vodka drinks.
+ * It is derived from the SimpleDrinks class and holds a Liquid object,
+ * that represents a liquid.
+ * <p>
+ * VodkaDrink is used to calculate the effects of alcoholic vodka drinks
+ * on the human body, based on size, weight, gender and duration of intake,
+ * given that drinks were taken in consecutively over that duration of time.
+ *
+ * @author Tobias Weigl
+ * @version %I%, %G%
+ */
+public class VodkaDrink {
+}
