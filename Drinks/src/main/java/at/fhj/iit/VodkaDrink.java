e265bdb20e24bdf84c467901153bec21f31fee0f
weTobias <tobias.weigl99@gmail.com>
2020-05-09 20:47:48 +0200

    Update VodkaDrink.java
    VodkaDrink:
    Updated description of calculateEffects().
    Added functionality to calculateEffects().

--- Drinks/src/main/java/at/fhj/iit/VodkaDrink.java
+++ Drinks/src/main/java/at/fhj/iit/VodkaDrink.java
@@ -31,12 +31,42 @@ public abstract class VodkaDrink extends SimpleDrink{
      * list of alcoholic drinks on the human body, based on weight,
      * gender and duration of intake.
      *
+     * @param time duration of intake in minutes
      * @param gender gender of the person which effects are being calculated
-     * @param weight weight of the person which effects are being calculated
+     * @param weight weight in grams of the person which effects are being calculated
      * @param list list of drinks that were taken in
      */
-    public static String calculateEffects(Gender gender, int weight, ArrayList<VodkaDrink> list){
-        return null;
+    public static String calculateEffects(int time, Gender gender, int weight, ArrayList<VodkaDrink> list){
+        double gramsOfAlcohol = 0;
+        for(VodkaDrink drink : list){
+            gramsOfAlcohol += (double) (drink.getVolume()*1000)/drink.getAlcoholPercent()*0.789;
+        }
+        double alcoholBloodPercentage = (gramsOfAlcohol/(weight * ((gender==Gender.MALE)?0.68:0.55)))*100;
+        alcoholBloodPercentage -= (gender==Gender.MALE)?0.015*(time/60):0.013*(time/60);
+
+        if(alcoholBloodPercentage > 0.02){
+            return "Tipsy";
+        }
+        else if(alcoholBloodPercentage > 0.05){
+            return "No driving";
+        }
+        else if(alcoholBloodPercentage > 0.1){
+            return "Drunk";
+        }
+        else if(alcoholBloodPercentage > 0.2){
+            return "Very drunk";
+        }
+        else if(alcoholBloodPercentage > 0.3){
+            return "Alcohol poisoning";
+        }
+        else if(alcoholBloodPercentage > 0.4){
+            return "Probably dead";
+        }
+        else if(alcoholBloodPercentage > 0.5){
+            return "You dead";
+        }
+        return "You good";
+
     }
 
 }
