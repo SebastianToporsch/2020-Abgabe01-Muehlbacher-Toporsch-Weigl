2a1838295c9b7c08334a43e022cd51db81635e95
weTobias <tobias.weigl99@gmail.com>
2020-05-09 20:59:54 +0200

    Update VodkaLongdrink.java
    VodkaLongdrink:
    Removed vodka and filler variables as they are not needed.
    Removed variables from constructor.
    Updated constructor description to show when an exception can occur.

--- Drinks/src/main/java/at/fhj/iit/VodkaLongdrink.java
+++ Drinks/src/main/java/at/fhj/iit/VodkaLongdrink.java
@@ -8,18 +8,11 @@ package at.fhj.iit;
  * @version %I%, %G%
  */
 public class VodkaLongdrink extends VodkaDrink{
-    /**
-     * represents vodka in longdrink
-     */
-    private Liquid vodka;
-    /**
-     * represents filler in longdrink
-     */
-    private Liquid filler;
 
     /**
      * Creates new longdrink with given name and liquids.
      * Mixes the two given liquids into a new one.
+     * An exception can occur when vodka and filler have a volume of 0, because of a division by 0.
      *
      * @param name name of the drink
      * @param vodka represents the vodka part of the longdrink
@@ -28,7 +21,5 @@ public class VodkaLongdrink extends VodkaDrink{
     public VodkaLongdrink(String name, Liquid vodka, Liquid filler){
         super(name, new Liquid(name, vodka.getVolume()+filler.getVolume(),
                 (vodka.getAlcoholPercent() * vodka.getVolume())/(vodka.getVolume()+filler.getVolume())));
-        this.vodka = vodka;
-        this.filler = filler;
     }
 }
