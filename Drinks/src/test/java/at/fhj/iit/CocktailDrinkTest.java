0adaf545779554989d0a62b89505228e322b5eff
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-10 18:42:01 +0200

    Update CocktailDrinkTest.java
    added DisplayName at the start fo CocktailDrinkTest

--- Drinks/src/test/java/at/fhj/iit/CocktailDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/CocktailDrinkTest.java
@@ -1,9 +1,11 @@
 package at.fhj.iit;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testing CocktailDrink implementation")
 class CocktailDrinkTest {
 
     @Test
