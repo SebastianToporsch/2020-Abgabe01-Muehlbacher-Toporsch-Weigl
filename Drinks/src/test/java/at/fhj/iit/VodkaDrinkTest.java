da4d382fb40d484c7dc3deaa21ba755f42e0620a
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-10 18:37:39 +0200

    Update VodkaDrinkTest.java
    Updating VodkaDrinkTest to have @BeforeEach

--- Drinks/src/test/java/at/fhj/iit/VodkaDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/VodkaDrinkTest.java
@@ -1,11 +1,20 @@
 package at.fhj.iit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class VodkaDrinkTest {
+    private VodkaLongdrink VL;
+    private VodkaShot VS;
 
+    @BeforeEach
+    void setup(){
+        //VL = new VodkaLongdrink();
+        //VS = new VodkaShot();
+
+    }
     @Test
     void calculateEffects() {
     }
