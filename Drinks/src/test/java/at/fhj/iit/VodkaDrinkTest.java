50358faf8d1dc0cda4ab4ecb96ec4ed4762f3de1
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-10 18:43:06 +0200

    Update VodkaDrinkTest.java
    Added DisplayName message in VodkaDrinkTest

--- Drinks/src/test/java/at/fhj/iit/VodkaDrinkTest.java
+++ Drinks/src/test/java/at/fhj/iit/VodkaDrinkTest.java
@@ -1,10 +1,12 @@
 package at.fhj.iit;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+@DisplayName("Testing VodkaDrink implementation")
 class VodkaDrinkTest {
     private VodkaLongdrink VL;
     private VodkaShot VS;
