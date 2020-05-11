7f074b86acb27e8f3278ada67dccb7fafa1abef1
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-12 00:06:37 +0200

    Update VodkaDrinkTest.java
    update calculateEffects test

--- Drinks/src/main/test/java/at/fhj/iit/VodkaDrinkTest.java
+++ Drinks/src/main/test/java/at/fhj/iit/VodkaDrinkTest.java
@@ -19,7 +19,7 @@ class VodkaDrinkTest {
     private Liquid Wasser = new Liquid("Wasser", 0.3, 0);
     private Liquid Wein = new Liquid("Wein", 0.125, 13);
     private Liquid Energy = new Liquid("Energy Drink", 0.33,0);
-    private Liquid VodkaBottle = new Liquid("Vodka Bottle", 0.2, 37.5);
+    private Liquid VodkaBottle = new Liquid("Vodka Bottle", 0.7, 37.5);
     private Liquid VodkaShot = new Liquid("Vodka Shot", 0.02, 37.5);
 
     @BeforeEach
@@ -35,21 +35,21 @@ class VodkaDrinkTest {
 
     @Test
     void calculateEffects() {
-        assertEquals(VodkaDrink.calculateEffects(15, Gender.MALE, 70, VodkaLiquids), "No driving");
-        //assertEquals(VodkaDrink.calculateEffects(90, Gender.MALE, 100, VodkaLiquids), "No driving");
+        assertEquals(VodkaDrink.calculateEffects(15, Gender.MALE, 70, VodkaLiquids), "Tipsy");
 
     }
 
     @Test
     void testGetAlcoholPercent() {
-        assertEquals(VL.getAlcoholPercent(), 28);
+        assertEquals(VL.getAlcoholPercent(), 37.5);
         assertEquals(VS.getAlcoholPercent(), 37.5);
 
     }
 
     @Test
     void testGetVolume() {
-        assertEquals(VL.getVolume(), 1);
+        assertEquals(VL.getVolume(), 0.7);
+        assertEquals(VS.getVolume(), 0.02);
     }
 
     @Test
