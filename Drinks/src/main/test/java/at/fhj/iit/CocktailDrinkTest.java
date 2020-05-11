7a60e0398f767b57bd31b6701edf78195b58ba38
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-11 09:17:43 +0200

    Update CocktailDrinkTest.java
    added Testcases to CocktailDrinkTest

--- Drinks/src/main/test/java/at/fhj/iit/CocktailDrinkTest.java
+++ Drinks/src/main/test/java/at/fhj/iit/CocktailDrinkTest.java
@@ -1,14 +1,27 @@
 package at.fhj.iit;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+
+
 @DisplayName("Testing CocktailDrink implementation")
 class CocktailDrinkTest {
+private CocktailDrink C1;
+private CocktailDrink C2;
+
+    @BeforeEach
+    void setup(){
+        C1 = new PinaColada();
+        C2 = new LongIslandIceTea();
+    }
     @Test
     void getCalories() {
-
+        assertEquals(C1.getCalories(),294.0);
+        assertEquals(C2.getCalories(),282.68000000000006);
     }
 }
