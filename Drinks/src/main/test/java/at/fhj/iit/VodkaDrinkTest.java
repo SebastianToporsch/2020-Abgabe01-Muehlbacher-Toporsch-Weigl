3670146a3accda077e851dd3386b4059fe5c4fe6
SebastianToporsch <sebastian.toporsch@edu.fh-joanneum.at>
2020-05-12 13:34:44 +0200

    Update VodkaDrinkTest.java

--- Drinks/src/main/test/java/at/fhj/iit/VodkaDrinkTest.java
+++ Drinks/src/main/test/java/at/fhj/iit/VodkaDrinkTest.java
@@ -15,11 +15,14 @@ class VodkaDrinkTest {
     private VodkaDrink VL;
     private VodkaShot VS;
     private VodkaShot BVS;
+    private VodkaShot EmptyShot;
     private VodkaDrink VodkaE;
+    private VodkaDrink EmptyDrink;
     private Liquid Energy = new Liquid("Energy Drink", 0.32,0);
     private Liquid VodkaBottle = new Liquid("Vodka Bottle", 0.7, 37.5);
     private Liquid VodkaShot = new Liquid("Vodka Shot", 0.02, 37.5);
     private Liquid BigVodkaShot = new Liquid("Big Vodka Shot",0.08,37.5);
+    private Liquid Nothing = new Liquid("Nothing",0,0);
 
     @BeforeEach
     void setup() {
@@ -27,9 +30,11 @@ class VodkaDrinkTest {
         VS = new VodkaShot(VodkaShot);
         BVS = new VodkaShot(BigVodkaShot);
         VodkaE = new VodkaLongdrink("VodkaE",VodkaLongdrink.createMix("VodkaE",BVS,Energy));
+        EmptyShot = new VodkaShot(Nothing);
     }
 
     @Test
+    @DisplayName("Testing calculateEffects Method")
     void calculateEffects() {
         VodkaLiquids.add(VS);
         VodkaLiquids.add(VS);
@@ -59,9 +64,30 @@ class VodkaDrinkTest {
 
         VodkaLiquids.add(VL);
         assertEquals(VodkaDrink.calculateEffects(60,Gender.MALE,100,VodkaLiquids),"Very drunk");
+        VodkaLiquids.clear();
+
+        VodkaLiquids.add(VS);
+        assertEquals(VodkaDrink.calculateEffects(1, Gender.MALE, 1, VodkaLiquids), "You dead");
+        VodkaLiquids.clear();
+
+        VodkaLiquids.add(VL);
+        VodkaLiquids.add(VL);
+        VodkaLiquids.add(VL);
+        assertEquals(VodkaDrink.calculateEffects(5, Gender.MALE, 150, VodkaLiquids), "You dead");
     }
 
     @Test
+    @DisplayName("Testing createMix Method")
+    void testCreateMix() {
+
+        assertEquals(EmptyDrink = new VodkaLongdrink("ErrorVodka", VodkaLongdrink.createMix("No mix", VS, Nothing)),EmptyDrink);
+
+        assertEquals(new VodkaLongdrink("ErrorVodka", VodkaLongdrink.createMix("No mix", EmptyShot, Nothing)),0 );
+
+    }
+
+    @Test
+    @DisplayName("Testing getAlcoholPercent Method")
     void testGetAlcoholPercent() {
         assertEquals(VL.getAlcoholPercent(), 37.5);
         assertEquals(VS.getAlcoholPercent(), 37.5);
@@ -70,6 +96,7 @@ class VodkaDrinkTest {
     }
 
     @Test
+    @DisplayName("Testing getVolume Method")
     void testGetVolume() {
         assertEquals(VL.getVolume(), 0.7);
         assertEquals(VS.getVolume(), 0.02);
@@ -78,10 +105,12 @@ class VodkaDrinkTest {
     }
 
     @Test
+    @DisplayName("Testing isAlcoholic Method")
     void testIsAlcoholic() {
         assertEquals(VL.isAlcoholic(), true);
         assertEquals(VS.isAlcoholic(), true);
         assertEquals(BVS.isAlcoholic(), true);
         assertEquals(VodkaE.isAlcoholic(),true);
+        assertEquals(EmptyShot.isAlcoholic(),false);
     }
 }
